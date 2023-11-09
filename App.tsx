e7b963375e25daacb00fdf08bd8f77d9523fec2a
ttl123412 <150228230+ttl123412@users.noreply.github.com>
2023-11-09 14:44:40 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Login from './screens/login'
+
+class MyApp extends Component {
+  render() {
+    return (
+      <Login />
+    )
+  }
+}
+
+export default MyApp
