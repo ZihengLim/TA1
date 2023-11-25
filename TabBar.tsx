16c88e6e4b7a946a528cca891cbd4e7b061f6117
ttl123412 <150228230+ttl123412@users.noreply.github.com>
2023-11-26 03:16:46 +0800

    Updated TabBar.tsx

--- TabBar.tsx
+++ TabBar.tsx
@@ -1,49 +1,72 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-//Screen names
-const homeName = "Home";
-const detailsName = "Details";
-const settingsName = "Settings";
-
-const Tab = createBottomTabNavigator();
-
-function MainContainer() {
+const MainPage = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={homeName}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let rn = route.name;
-
-            if (rn === homeName) {
-              iconName = focused ? 'home' : 'home-outline';
-
-            } else if (rn === detailsName) {
-              iconName = focused ? 'list' : 'list-outline';
-
-            } else if (rn === settingsName) {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'grey',
-          labelStyle: { paddingBottom: 10, fontSize: 10 },
-          style: { padding: 10, height: 70}
-        }}>
-
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Main Page</Text>
+      </View>
+      <View style={styles.content}>
+        {/* put the content here */}
+      </View>
+      <View style={styles.tabBar}>
+        <TouchableOpacity style={styles.tabItem}>
+          <Icon name="home-outline" size={25} />
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem}>
+          <Icon name="list-outline" size={25} />
+          <Text>My Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem}>
+          <Icon name="add-circle-outline" size={25} />
+          <Text>Post Task</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem}>
+          <Icon name="chatbubble-outline" size={25} />
+          <Text>Messages</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem}>
+          <Icon name="settings-outline" size={25} />
+          <Text>Setting</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
-export default MainContainer;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff', // Set the background color for the entire container
+  },
+  header: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  headerText: {
+    fontSize: 20,
+  },
+  content: {
+    flex: 1,
+    // Add content styles here
+  },
+  tabBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    backgroundColor: '#f9f9f9', 
+  },
+  tabItem: {
+    alignItems: 'center',
+    padding: 10,
+  },
+});
+
+export default MainPage;
