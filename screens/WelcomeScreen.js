52f1beb1cbaf9f9473143ae6e1c497612f7432bb
ttl123412 <150228230+ttl123412@users.noreply.github.com>
2024-01-16 21:43:23 +0800

    Update WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import {GeneralButton} from '../assets/components/Buttons';
 import {Image, StyleSheet, Text, View} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {LoginRegisterButtons} from './assets/components/LoginRegisterButtons';
 
 export default function WelcomeScreen() {
-  const navigation = useNavigation();
-
   return (
     <View style={styles.welcomeScreenContainer}>
-      <View style={styles.welcomeScreenImageContainer}>
+      <View style={styles.imageContainer}>
         <Image
           style={styles.welcomeScreenImage}
-          source={require('../assets/images/WelcomeScreen.png')}
+          source={require('./assets/images/WelcomeScreen.png')}
         />
       </View>
-      <View style={styles.welcomeTextContainer}>
-        <Text style={styles.welcomeText}>Welcome to Taskway</Text>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.welcomeText}>Welcome to TaskAway</Text>
         <View style={styles.buttonContainer}>
-          <GeneralButton
-            buttonType="LOGIN"
-            onPress={() => navigation.navigate('Login')}
-          />
-          <GeneralButton
-            buttonType="REGISTER"
-            onPress={() => navigation.navigate('SignUp')}
-          />
+          <LoginRegisterButtons buttonType={'LOGIN'} />
+          <LoginRegisterButtons buttonType={'REGISTER'} />
         </View>
       </View>
     </View>
@@ -36,31 +27,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
   },
-  welcomeScreenImageContainer: {
-    alignItems: 'center',
+  imageContainer: {
+    flex: 0.75,
+    marginVertical: 30,
   },
   welcomeScreenImage: {
-    marginBottom: 20,
-    height: 665,
-    width: 420,
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
   },
-  welcomeTextContainer: {
-    flex: 1,
+  bottomContainer: {
+    flex: 0.25,
     backgroundColor: '#FDAB2F',
     borderTopLeftRadius: 35,
     borderTopRightRadius: 35,
     alignItems: 'center',
-    padding: 20,
   },
   welcomeText: {
+    marginVertical: 30,
     fontSize: 30,
-    color: '#000000',
     fontWeight: 'bold',
-    marginBottom: 35,
+    color: '#000000',
   },
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
   },
 });
+
