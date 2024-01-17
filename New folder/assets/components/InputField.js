cfdc619361627c329cb9b0670e3afcc8cac4d83d
ttl123412 <150228230+ttl123412@users.noreply.github.com>
2024-01-18 07:21:03 +0800

    Update InputField.js

--- New folder/assets/components/InputField.js
+++ New folder/assets/components/InputField.js
@@ -1,72 +1,56 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Entypo';
-const EmailInputField = () => (
-  <>
-    <Text style={styles.inputFieldTitle}>E-MAIL:</Text>
-    <TextInput
-      style={styles.inputField}
-      placeholder="example@gmail.com"
-      placeholderTextColor="#A19393"
-      selectionColor="#000000"
-    />
-  </>
-);
+import React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, Text, TextInput} from 'react-native';
 
-const PasswordInputField = () => {
-  const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
+const InputField = ({label, placeholder, isSecure}) => {
   return (
     <>
-      <Text style={styles.inputFieldTitle}>PASSWORD:</Text>
-      <View style={styles.passwordField}>
-        <TextInput
-          style={styles.inputField}
-          placeholder="********"
-          placeholderTextColor="#A19393"
-          selectionColor="#000000"
-          secureTextEntry={!passwordVisible} // Toggle visibility
-        />
-        <TouchableOpacity onPress={togglePasswordVisibility}>
-          <Icon
-            name={passwordVisible ? 'eye-with-line' : 'eye'}
-            size={25}
-            color="black"
-          />
-        </TouchableOpacity>
-      </View>
+      <Text style={styles.inputLabel}>{label}</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder={placeholder}
+        placeholderTextColor="#A19393"
+        selectionColor="#000000"
+        secureTextEntry={isSecure}
+        keyboardType={isSecure ? 'default' : 'email-address'}
+        autoCapitalize="none"
+      />
     </>
   );
 };
 
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  isSecure: PropTypes.bool,
+};
+
 const styles = StyleSheet.create({
-  inputFieldTitle: {
+  inputLabel: {
     fontSize: 15,
     color: '#000000',
     fontWeight: 'bold',
     marginVertical: 10,
   },
   inputField: {
-    marginBottom: 10,
-  },
-  passwordField: {
-    flexDirection: 'row',
     borderColor: '#120D92',
-    paddingHorizontal: 10,
+    marginBottom: 15,
     borderWidth: 1.5,
     borderRadius: 5,
-    alignItems: 'center',
+    height: 60,
+    paddingHorizontal: 10,
   },
 });
 
-export {EmailInputField, PasswordInputField};
+export const EmailInputField = () => (
+  <InputField
+    label="E-MAIL:"
+    placeholder="example@gmail.com"
+    isSecure={false}
+  />
+);
+
+export const PasswordInputField = () => (
+  <InputField label="PASSWORD:" placeholder="********" isSecure={true} />
+);
+
