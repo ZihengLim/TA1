6f333abe1d195044bc228a9970477bd117a68ce7
ttl123412 <150228230+ttl123412@users.noreply.github.com>
2024-01-17 19:02:41 +0800

    Update InputField.js

--- assets/components/InputField.js
+++ assets/components/InputField.js
@@ -1,44 +1,56 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import PropTypes from 'prop-types';
 
-const EmailInputField = () => (
-  <>
-    <Text style={styles.inputName}>E-MAIL:</Text>
-    <TextInput
-      style={styles.inputField}
-      placeholder="example@gmail.com"
-      placeholderTextColor="#A19393"
-      selectionColor="#000000"
-    />
-  </>
-);
-
-const PasswordInputField = () => (
-  <>
-    <Text style={styles.inputName}>PASSWORD:</Text>
-    <TextInput
-      style={styles.inputField}
-      placeholder="********"
-      placeholderTextColor="#A19393"
-      selectionColor="#000000"
-    />
-  </>
-);
+const InputField = ({label, placeholder, isSecure}) => {
+  return (
+    <>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputName}>{label}</Text>
+        <TextInput
+          style={styles.inputField}
+          placeholder={placeholder}
+          placeholderTextColor="#A19393"
+          selectionColor="#000000"
+          secureTextEntry={isSecure}
+          keyboardType={isSecure ? 'default' : 'email-address'}
+          autoCapitalize="none"
+        />
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   inputName: {
     fontSize: 15,
-    marginBottom: 10,
     color: '#000000',
     fontWeight: 'bold',
   },
   inputField: {
-    paddingHorizontal: 10,
-    marginBottom: 20,
     borderColor: '#120D92',
     borderWidth: 1.5,
     borderRadius: 5,
+    height: 40,
+    paddingHorizontal: 10,
   },
 });
 
-export {EmailInputField, PasswordInputField};
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  isSecure: PropTypes.bool,
+};
+
+export const EmailInputField = () => (
+  <InputField
+    label="E-MAIL:"
+    placeholder="example@gmail.com"
+    isSecure={false}
+  />
+);
+
+export const PasswordInputField = () => (
+  <InputField label="PASSWORD:" placeholder="********" isSecure={true} />
+);
+
